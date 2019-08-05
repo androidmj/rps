b85fbb3bca612edcfa81443b162b719abe634f46
androidmj <dietcocacola@gmail.com>
2019-08-04 20:50:33 -0400

    Some homepage changes in preparation for video hero.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -33,6 +33,30 @@ class HomepageController extends BaseController
         $render_data['page_description'] = $main_head->description;
         $render_data['page_og_image'] = $main_head->og_image;
 
+        $render_data['hero_data'][] = array('h1'=>'Secondary Equipment, Cobots, Assembly, Integration, and Robotics',
+            'h2' => 'From seasoned workers who take pride<br>in producing the best product possible.',
+            'link' => '/about',
+            'link_text' => 'Get More with RPS',
+            'link_title' => 'RPS cobots',
+            'button_color' => 'btn-green',
+            'background_image' => 'images/cobots-banner.png');
+
+        $render_data['hero_data'][] = array('h1'=>'Robots with the Best Speed, Precision, and Productivity',
+            'h2' => 'We build automation equipment using the best robots that are ideal for high-speed, precision applications such as assembly, pick and place, testing/inspection, and packing processes',
+            'link' => '/about',
+            'link_text' => 'Get More with RPS',
+            'link_title' => 'RPS cobots',
+            'button_color' => 'btn-yellow',
+            'background_image' => 'images/fanuc-scara.jpg');
+
+        $render_data['hero_data'][] = array('h1'=>'ndustrial Robots for Manufacturing',
+            'h2' => 'Versatility, reliability and world-renowned performance - we use robots that provide manufacturers worldwide with the productivity they need to improve performance and profitability',
+            'link' => '/about',
+            'link_text' => 'Get More with RPS',
+            'link_title' => 'RPS cobots',
+            'button_color' => 'btn-yellow',
+            'background_image' => 'images/robots-for-maufacturing.jpg');
+
         return $this->render('default/homepage.html.twig', $render_data);
     }
 }
