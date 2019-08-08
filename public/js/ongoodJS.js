f42c540e280ea77e0854fd120ceb2b8cc4d7e81d
androidmj <dietcocacola@gmail.com>
2019-08-08 01:27:06 -0400

    Pruning unused files from previous projects

--- public/js/ongoodJS.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.jqTransformSelectWrapper').off('click').on('click', function () {
-    $(this).find('ul').toggle('fast');
-  });
-
-  $('.jqTransformSelectWrapper').find('li').off('click').on('click', function () {
-    $(this).find('div').find('span').html('asdf');
-  });
-});
